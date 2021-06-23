3ae98d51310d1511e4db2c6571d88e7c4725c349
末城via <969954321@qq.com>
2021-06-23 22:36:11 +0800

    Pagination

---
+++ DotNet/WPF/Src/SharedResources/Panuon.UI.Core/Events/SelectedValueChangedEventArgs`.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Panuon.UI.Core
+{
+    public class SelectedValueChangedEventArgs<T> : RoutedEventArgs
+    {
+        #region Ctor
+        public SelectedValueChangedEventArgs(RoutedEvent routedEvent, T newValue, T oldValue)
+            : base(routedEvent)
+        {
+            NewValue = newValue;
+            OldValue = oldValue;
+        }
+        #endregion
+
+        #region Properties
+        public T NewValue { get; }
+
+        public T OldValue { get; }
+        #endregion
+    }
+}
---
+++ DotNet/WPF/Src/SharedResources/Panuon.UI.Silver.Internal/Converters/DoubleEqualsConverter.cs
@@ -0,0 +1,25 @@
+﻿using Panuon.UI.Core;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Panuon.UI.Silver.Internal.Converters
+{
+    public class DoubleEqualsConverter : MultiValueConverterBase
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var lastValue = double.Parse(values[0].ToString());
+            foreach(var value in values)
+            {
+                var doubleValue = double.Parse(value.ToString());
+                if(lastValue != doubleValue)
+                {
+                    return false;
+                }
+                lastValue = doubleValue;
+            }
+            return true;
+        }
+    }
+}
