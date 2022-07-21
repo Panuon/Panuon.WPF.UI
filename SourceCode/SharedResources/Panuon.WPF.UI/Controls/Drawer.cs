55e484a56e3a4aec0b0ac71606323f4e49d3f0ce
末城via <mochengvia@panuon.com>
2022-07-21 21:33:32 +0800

    1.0.4

--- SourceCode/SharedResources/Panuon.WPF.UI/Controls/Drawer.cs
+++ SourceCode/SharedResources/Panuon.WPF.UI/Controls/Drawer.cs
@@ -149,7 +149,7 @@ namespace Panuon.WPF.UI
         public void Open()
         {
             if (ActualWidth != 0
-                && ActualHeight != 0)
+                || ActualHeight != 0)
             {
 
                 if (!StaysOpen)
@@ -184,7 +184,7 @@ namespace Panuon.WPF.UI
         public void Close()
         {
             if (ActualWidth != 0
-                && ActualHeight != 0)
+                || ActualHeight != 0)
             {
                 switch (Placement)
                 {
