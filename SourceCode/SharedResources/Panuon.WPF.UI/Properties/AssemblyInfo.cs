196aa69efd0507c16b2a3b66324c2d37161ac5c0
末城via <mochengvia@panuon.com>
2023-05-30 10:57:48 +0800

    1.1.11.10

--- SourceCode/SharedResources/Panuon.WPF.UI/Properties/AssemblyInfo.cs
+++ SourceCode/SharedResources/Panuon.WPF.UI/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Windows.Markup;
     ResourceDictionaryLocation.SourceAssembly
 )]
 
-[assembly: AssemblyVersion("1.1.11.9")]
-[assembly: AssemblyFileVersion("1.1.11.9")]
+[assembly: AssemblyVersion("1.1.11.10")]
+[assembly: AssemblyFileVersion("1.1.11.10")]
