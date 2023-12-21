98978043cf2a3e3b7fa9a1181cc3b7ab81f194c2
末城via <mochengvia@panuon.com>
2023-12-21 15:18:40 +0800

    watermark helper

---
+++ SourceCode/SharedResources/Panuon.WPF.UI/Helpers/WatermarkHelper.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Panuon.WPF.UI
+{
+    public static class WatermarkHelper
+    {
+        #region Properties
+
+        #region Margin
+        public static Thickness GetMargin(DependencyObject obj)
+        {
+            return (Thickness)obj.GetValue(MarginProperty);
+        }
+
+        public static void SetMargin(DependencyObject obj, Thickness value)
+        {
+            obj.SetValue(MarginProperty, value);
+        }
+
+        public static readonly DependencyProperty MarginProperty =
+            DependencyProperty.RegisterAttached("Margin", typeof(Thickness), typeof(WatermarkHelper), new PropertyMetadata(default(Thickness)));
+        #endregion
+
+        #region FontSize
+        public static double GetFontSize(DependencyObject obj)
+        {
+            return (double)obj.GetValue(FontSizeProperty);
+        }
+
+        public static void SetFontSize(DependencyObject obj, double value)
+        {
+            obj.SetValue(FontSizeProperty, value);
+        }
+
+        public static readonly DependencyProperty FontSizeProperty =
+            DependencyProperty.RegisterAttached("FontSize", typeof(double), typeof(WatermarkHelper), new PropertyMetadata(12.0));
+        #endregion
+
+        #region FontFamily
+        public static FontFamily GetFontFamily(DependencyObject obj)
+        {
+            return (FontFamily)obj.GetValue(FontFamilyProperty);
+        }
+
+        public static void SetFontFamily(DependencyObject obj, FontFamily value)
+        {
+            obj.SetValue(FontFamilyProperty, value);
+        }
+
+        public static readonly DependencyProperty FontFamilyProperty =
+            DependencyProperty.RegisterAttached("FontFamily", typeof(FontFamily), typeof(WatermarkHelper), new PropertyMetadata(SystemFonts.MessageFontFamily));
+        #endregion
+
+        #endregion
+    }
+
+}
